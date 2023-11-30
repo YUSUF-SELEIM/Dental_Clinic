e4d18859903e588ec3a111e73646cd935f8bd5a6
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2023-12-01 01:25:07 +0200

    Added tabs_manager.js to manage tabs in user's page

---
+++ src/js/modules/tabs_manager.js
@@ -0,0 +1,54 @@
+import { Tabs } from 'flowbite';
+    
+    const tabsElement = document.getElementById('tabs-example');
+    
+    // create an array of objects with the id, trigger element (eg. button), and the content element
+    const tabElements = [
+        {
+            id: 'profile',
+            triggerEl: document.querySelector('#profile-tab-example'),
+            targetEl: document.querySelector('#profile-example'),
+        },
+        {
+            id: 'dashboard',
+            triggerEl: document.querySelector('#dashboard-tab-example'),
+            targetEl: document.querySelector('#dashboard-example'),
+        },
+        {
+            id: 'settings',
+            triggerEl: document.querySelector('#settings-tab-example'),
+            targetEl: document.querySelector('#settings-example'),
+        },
+        {
+            id: 'contacts',
+            triggerEl: document.querySelector('#contacts-tab-example'),
+            targetEl: document.querySelector('#contacts-example'),
+        },
+    ];
+    
+    // options with default values
+    const options = {
+        defaultTabId: 'settings',
+        activeClasses:
+            'text-blue-600 hover:text-blue-600 dark:text-blue-500 dark:hover:text-blue-400 border-blue-600 dark:border-blue-500',
+        inactiveClasses:
+            'text-gray-500 hover:text-gray-600 dark:text-gray-400 border-gray-100 hover:border-gray-300 dark:border-gray-700 dark:hover:text-gray-300',
+        onShow: () => {
+            console.log('tab is shown');
+        },
+    };
+    
+    // instance options with default values
+    const instanceOptions = {
+      id: 'tabs-example',
+      override: true
+    };
+    
+    /*
+    * tabElements: array of tab objects
+    * options: optional
+    * instanceOptions: optional
+    */
+    const tabs = new Tabs(tabsElement, tabElements, options, instanceOptions);
+    tabs.show('dashboard');
+    
