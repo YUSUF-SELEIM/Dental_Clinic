e64e0eae6ec76c200d6f32628707d7f5c5f3e557
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2023-12-04 03:59:42 +0200

    added green check when user has already booked added after booking the page redirects to itself

--- src/php/user_page.php
+++ src/php/user_page.php
@@ -64,6 +64,7 @@ if (isset($_POST['submit'])) {
         if (mysqli_query($conn, $update_user_bookedOrNot_query)) {
             $_SESSION['user_id'] = $user_id ;
             echo "Sent";
+            header("Location: user_page.php");
             exit(); // Make sure to exit to prevent further execution
         } else {
             echo "Error updating user status: " . mysqli_error($conn);
@@ -197,7 +198,10 @@ if (isset($_POST['log-out'])) {
                 </div>
             </div>
             <div id="booking-confirmation" class="hidden rounded-lg bg-gray-50 p-4 dark:bg-gray-800"  >
-                <p class="text-sm text-gray-500 dark:text-gray-400">
+            <div class="w-12 h-12 rounded-full bg-green-100 dark:bg-green-900 p-2 flex items-center justify-center mx-auto mb-3.5">
+                <svg aria-hidden="true" class="w-8 h-8 text-green-500 dark:text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
+            </div>   
+            <p class="text-sm text-gray-500 dark:text-gray-400">
                     You have successfully booked an appointment. Your booking is awaiting admin approval.
                 </p>
             </div>
